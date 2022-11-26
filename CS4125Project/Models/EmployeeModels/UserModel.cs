4b62a70e8d1f236910a02e42063e043a3fdafa5d
Josh McGiff <jbmcgiff@gmail.com>
2022-11-26 13:54:43 +0000

    Added tests to user model

--- CS4125Project/Models/EmployeeModels/UserModel.cs
+++ CS4125Project/Models/EmployeeModels/UserModel.cs
@@ -1,10 +1,45 @@
-﻿namespace CS4125Project.Models.EmployeeModels
+﻿using System.Diagnostics.Contracts;
+
+namespace CS4125Project.Models.EmployeeModels
 {
     public class UserModel
     {
-        public string name { get; set; }
-        public int id { get; set; }
-        public string email { get; set; }
+
+        public string name { 
+
+            get { 
+                return name; 
+            } 
+
+            set { 
+                Contract.Requires(value.Split(" ").Length > 1); 
+            } 
+
+        }
+
+        public int id { 
+
+            get { 
+                return id;  
+            } 
+            
+            set { 
+                Contract.Requires(value > 0); 
+            } 
+
+        }
+
+        public string email { 
+            
+            get { 
+                return email; 
+            }  
+            
+            set { 
+                Contract.Requires(value.Contains("@") && value.Contains(".")); 
+            } 
+
+        }
 
     }
 }
